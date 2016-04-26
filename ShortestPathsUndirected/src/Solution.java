63a9b60692cea42a8eb5b6664e706745aebb54bc
Jarryd Goodman <jarryd999@gmail.com>
2016-04-26 13:57:45 -0700

    Started shortest path

---
+++ ShortestPathsUndirected/src/Solution.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int testCount = in.nextInt();
+		
+		
+		//for each test case:
+		for (int i = 0; i < testCount; i++){
+			
+		}
+
+	}
+
+}
