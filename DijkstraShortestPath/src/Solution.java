dd7cd33bc760531885ce971589366451b248b394
Jarryd Goodman <jarryd999@gmail.com>
2016-04-27 12:56:39 -0700

    final version, for some reason scanner breaks locally and won't read last line

--- DijkstraShortestPath/src/Solution.java
+++ DijkstraShortestPath/src/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 
-		int testCount = in.nextInt();
+		int testCount = Integer.parseInt(in.nextLine());
 
 		// for each test case:
 		for (int i = 0; i < testCount; i++) {
@@ -18,35 +18,68 @@ public class Solution {
 			boolean[][] matrix = new boolean[vCount][vCount];
 
 			// fill out the adjacency matrix
-			for (int j = 1; j < vCount; j++) {
-				int s = in.nextInt();
-				int t = in.nextInt();
+			for (int j = 0; j < eCount; j++) {
+				int s = in.nextInt() - 1;
+				int t = in.nextInt() - 1;
 
 				matrix[s][t] = true;
 				matrix[t][s] = true;
 			}
-			
-			int source = in.nextInt();
-			
-			int[] shortestPaths = new int[vCount - 1];
-			for (int j = 0; j < shortestPaths.length; j++){
+
+			int source = in.nextInt() - 1;
+
+			int[] shortestPaths = new int[vCount];
+			for (int j = 0; j < shortestPaths.length; j++) {
 				shortestPaths[j] = Integer.MAX_VALUE;
 			}
-			
+
 			HashSet<Integer> visitedNodes = new HashSet<Integer>();
-			LinkedList<Integer> queue = new LinkedList<Integer>();
-			queue.add(source);
-			while (!queue.isEmpty()){
-				int curr = queue.poll();
-				visitedNodes.add(curr);
-				
-				for (int j = 0; j < vCount; j++){
-					if (matrix[curr][j] && !visitedNodes.contains(j)){
-						queue.add(j);
+			LinkedList<LinkedList<Integer>> queue = new LinkedList<LinkedList<Integer>>();
+			LinkedList<Integer> curr = new LinkedList<Integer>();
+			curr.add(source);
+			curr.add(0);
+			queue.add(curr);
+			while (!queue.isEmpty()) {
+				curr = queue.poll();
+
+				// extract node # and depth from the queue
+				int node = curr.poll();
+				int depth = curr.poll();
+
+				// add all adjacents to queue
+				for (int j = 0; j < vCount; j++) {
+					if (node != j && matrix[node][j] && !visitedNodes.contains(j)) {
+
+						curr.add(j);
+						curr.add(depth + 1);
+						
+						if (shortestPaths[j] > depth + 1){
+							shortestPaths[j] = depth + 1;
+						}
+						queue.add(curr);
 					}
 				}
+				
+				// add node to visited nodes set
+				visitedNodes.add(node);
 			}
+			
+			StringBuffer out = new StringBuffer();
+			for (int j = 0; j < shortestPaths.length; j++){
+				if (j == source)
+					continue;
+				int dist = shortestPaths[j];
+				if (dist == Integer.MAX_VALUE){
+					out.append(" -1");
+				}else{
+					dist *= 6;
+					out.append(" " + dist);
+				}
+			}
+			
+			System.out.println(out.toString().trim());
 		}
+		in.close();
 
 	}
 
