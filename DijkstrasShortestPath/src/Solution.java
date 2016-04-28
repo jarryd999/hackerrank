f3ef779e23935b10da182b22181af28311b95b7a
Jarryd Goodman <jarryd999@gmail.com>
2016-04-28 14:33:05 -0700

    working Dijkstras

--- DijkstrasShortestPath/src/Solution.java
+++ DijkstrasShortestPath/src/Solution.java
@@ -1,6 +1,7 @@
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
@@ -12,10 +13,9 @@ public class Solution {
 		int testCount = in.nextInt();
 
 		Vertex[] vertexes;
-		boolean[] visited;
 		PriorityQueue<Vertex> pq;
 
-		Comparator<Vertex> cmp = new EdgeComparator();
+		// Comparator<Vertex> cmp = new EdgeComparator();
 
 		// loop through all test cases
 		for (int test = 0; test < testCount; test++) {
@@ -23,74 +23,121 @@ public class Solution {
 			int eCount = in.nextInt();
 
 			vertexes = new Vertex[vCount];
-			visited = new boolean[vCount];
-			pq = new PriorityQueue<Vertex>(vCount, cmp);
+
+			for (int i = 0; i < vCount; i++) {
+				vertexes[i] = new Vertex();
+			}
+			pq = new PriorityQueue<Vertex>(vCount);
 
 			// loop through input and add all edges
 			for (int e = 0; e < eCount; e++) {
 				parseEdge(in, vertexes, pq);
 			}
 
-			int source = in.nextInt();
+			// grab the source vertex
+			int source = in.nextInt() - 1;
+			vertexes[source].distance = 0;
 
-
+			// add vertexes to PriorityQueue
+			fillPriorityQueue(pq, vertexes);
 
 			// loop through until the PQ is empty
 			while (!pq.isEmpty()) {
-//				Integer[] curr = pq.poll();
-//				int node = curr[0];
-//
-//				int[] adjacents = matrix[node];
-//
-//				// loop through all neighbors
-//				for (int i = 0; i < adjacents.length; i++) {
-//					// if there exists an edge from s to i
-//					if (adjacents[i] > 0) {
-//						if ()
-//					}
-//				}
 
+				// extract the min
+				Vertex curr = pq.poll();
+				int currDist = curr.distance;
+				HashMap<Integer, Integer> adjacents = curr.adjacents;
+
+				// loop through all neighbors
+				Iterator<?> iterator = adjacents.entrySet().iterator();
+				while (iterator.hasNext()) {
+					Map.Entry pair = (Map.Entry) iterator.next();
+					int vId = (int) pair.getKey();
+					Vertex neighbor = vertexes[vId];
+					int neighborDist = neighbor.distance;
+					int edgeDist = (int) pair.getValue();
+
+					// if distance b/w curr node & neighbor is less than
+					// neighbor's
+					// distance, update
+					if (currDist != Integer.MAX_VALUE && neighborDist > currDist + edgeDist) {
+						vertexes[vId].distance = currDist + edgeDist;
+						pq.remove(vertexes[vId]);
+						pq.add(vertexes[vId]);
+					}
+					iterator.remove();
+				}
 			}
+
+			printResults(vertexes, source);
+
 		}
 	}
 
-	private static void parseEdge(Scanner in, Vertex[] vertexes, PriorityQueue<Vertex> pq) {
-		int s = in.nextInt();
-		int t = in.nextInt();
-		int weight = in.nextInt();
+	private static void fillPriorityQueue(PriorityQueue<Vertex> pq, Vertex[] vertexes) {
+		for (int i = 0; i < vertexes.length; i++) {
+			pq.add(vertexes[i]);
+		}
 
+	}
+
+	private static void printResults(Vertex[] vertexes, int source) {
+		StringBuffer buff = new StringBuffer();
+		// loop through and print all the distances
+		for (int i = 0; i < vertexes.length; i++) {
+			if (i == source)
+				continue;
+			Vertex v = vertexes[i];
+			if (v.distance == Integer.MAX_VALUE)
+				buff.append("-1 ");
+			else
+				buff.append(v.distance + " ");
+		}
+
+		System.out.println(buff.toString().trim());
+	}
+
+	private static void parseEdge(Scanner in, Vertex[] vertexes, PriorityQueue<Vertex> pq) {
+		int s = in.nextInt() - 1;
+		int t = in.nextInt() - 1;
+		int weight = in.nextInt();
+		int oldVal;
 		Vertex node1 = vertexes[s];
 		Vertex node2 = vertexes[t];
+
+		if (node1.adjacents.containsKey(t)){
+			oldVal = node1.adjacents.get(t);
+		}else
+			oldVal = Integer.MAX_VALUE;
 		
-		node1.addNeighbor(s, weight);
-		node2.addNeighbor(t, weight);
-
-		pq.add(node1);
-		pq.add(node2);
+		if (weight < oldVal) {
+			node1.addNeighbor(t, weight);
+			node2.addNeighbor(s, weight);
+		}
 	}
 }
 
-class EdgeComparator implements Comparator<Vertex> {
-	@Override
-	public int compare(Vertex o1, Vertex o2) {
-		return (o1.distance - o2.distance);
-	}
-}
 
-class Vertex {
+class Vertex implements Comparable<Vertex> {
 	public HashMap<Integer, Integer> adjacents;
 	public int distance;
-	
-	public Vertex(){
+
+	public Vertex() {
 		this.adjacents = new HashMap<Integer, Integer>();
 		this.distance = Integer.MAX_VALUE;
 	}
-	
-	public void addNeighbor(int n, int weight){
+
+	public void addNeighbor(int n, int weight) {
 		this.adjacents.put(n, weight);
 	}
-	
-	public void setDistance(int n){
+
+	public void setDistance(int n) {
 		this.distance = n;
+	}
+
+	@Override
+	public int compareTo(Vertex o) {
+		return (this.distance - o.distance);
 	}
 }
